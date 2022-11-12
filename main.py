8a9f7970a8ea055a23b7d7ef05347d37e7c4ca9b
Sourcery AI <>
2022-11-12 22:37:16 +0000

    'Refactored by Sourcery'

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ import logging
 
 def main():
   path = os.environ["PATH_INFO"]
-  qs = os.getenv("QUERY_STRING")
-  if qs:
-    path += "?" + qs
-  referer = os.environ.get("HTTP_REFERER")
-  if referer:
+  if qs := os.getenv("QUERY_STRING"):
+    path += f"?{qs}"
+  if referer := os.environ.get("HTTP_REFERER"):
     logging.info("Request %s from %s", path, referer)
   else:
     logging.info("Request %s", path)
   for key, value in sorted(os.environ.iteritems()):
     logging.debug("%s=%r", key, value)
-  print "Status: 404 Not found"
+  path = os.environ["PATH_INFO"]
 
 if __name__ == '__main__':
   main()
